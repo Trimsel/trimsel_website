35a4b38ddd9f6236733737b411f038bb037ba665
Balajikailash88 <balaji@minitzon.com>
2025-04-16 23:17:34 +0530

    optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  experimental: {
+    optimizeCss: true, // ✅ This enables CSS optimization
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
