c13caf6083b3dd7845fcdb6ec50a53a6f95916f1
Balajikailash88 <balaji@minitzon.com>
2025-04-22 20:46:25 +0530

    disallow few pages

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,42 @@
 // next-sitemap.config.js
-
 module.exports = {
     siteUrl: 'https://www.trimsel.com',
     generateRobotsTxt: true,
-    sitemapSize: 7000,
     changefreq: 'weekly',
     priority: 0.7,
-    exclude: ['/thank-you', '/blog', '/blockchain-development', '/portfolio', '/portfolio/ezyhelpers-case-study', '/portfolio/kariot-case-study', '/portfolio/mocial-case-study', '/portfolio/xaber-case-study', '/quality-engineering-testing'], // exclude routes if needed
+    sitemapSize: 7000,
+    exclude: [
+      '/thank-you',
+      '/blog',
+      '/blockchain-development',
+      '/portfolio',
+      '/portfolio/ezyhelpers-case-study',
+      '/portfolio/kariot-case-study',
+      '/portfolio/mocial-case-study',
+      '/portfolio/xaber-case-study',
+      '/quality-engineering-testing'
+    ],
+    robotsTxtOptions: {
+      policies: [
+        {
+          userAgent: '*',
+          allow: '/',
+          disallow: [
+            '/thank-you',
+            '/blog',
+            '/blockchain-development',
+            '/portfolio',
+            '/portfolio/ezyhelpers-case-study',
+            '/portfolio/kariot-case-study',
+            '/portfolio/mocial-case-study',
+            '/portfolio/xaber-case-study',
+            '/quality-engineering-testing'
+          ],
+        },
+      ],
+      additionalSitemaps: [
+        'https://www.trimsel.com/sitemap.xml',
+      ],
+    },
   };
   
